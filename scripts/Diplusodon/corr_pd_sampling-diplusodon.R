2d467ffb903ec12685ef4fb5131d18ef6707e6f6
Yago Barros-Souza <souzayagob@gmail.com>
2022-08-09 17:18:44 -0300

    Running correlation analyses
    PD ~ Redundancy

---
+++ scripts/Diplusodon/corr_pd_sampling-diplusodon.R
@@ -0,0 +1,59 @@
+#=====================================================================================================#
+
+library(raster)
+library(rgdal)
+library(tidyverse)
+library(ape)
+
+# Set wd
+setwd("B:/yagob/GoogleDrive/Academia/R-MSc")
+
+#=====================================================================================================#
+
+#=======#
+# INPUT #
+#=======#
+
+# Reading clean dataset
+coords <- read.csv(file = "datasets/Diplusodon/diplusodon_cr.csv", na.strings = c("", NA))
+
+# Reading PD results
+pd <- read.csv("results/Diplusodon/pd_stats.csv")
+pd <- pd %>% rename("id_grid" = id)
+
+# Reading tree
+tree <- read.nexus("trees/Diplusodon/pruned_tree-diplusodon.nex")
+
+# Removing from the dataset all species that are not represented in the tree
+coords <- coords[coords$gen_sp %in% tree$tip.label, ]
+
+# Data frames with all samples (all_samples) and with one sample per species per grid (one_sample)
+all_samples <- coords[ , which(colnames(coords) %in% c("gen_sp", "id_grid"))]
+one_sample <- unique(all_samples, by = c("gen_sp", "id_grid"))
+
+# Redundancy values
+redundancy <- tibble("id_grid" = unique(all_samples$id_grid), "richness" = NA, "n" = NA,
+                     "redundancy" = NA)
+
+SR <- plyr::count(one_sample$id_grid)
+N <- plyr::count(all_samples$id_grid)
+
+for(i in 1:nrow(redundancy)){
+  redundancy$richness[i] <- SR$freq[SR$x == redundancy$id_grid[i]]
+  redundancy$n[i] <- N$freq[N$x == redundancy$id_grid[i]]
+}
+
+redundancy$redundancy <- 1-(redundancy$richness/redundancy$n)
+
+# Merging redundancy and PD
+pd_red <- merge(pd, redundancy, by = "id_grid")
+
+corrPdRed <- as.character(formatC(cor(pd_red$pd.obs, 
+                                     pd_red$redundancy, use = "na.or.complete"))) 
+corrPdRed_plot <- ggplot(data = pd_red, mapping = aes(jitter(redundancy), pd.obs))+
+  geom_jitter()+
+  labs(title = "Diplusodon", subtitle = paste("r =", corrPdRed))+
+  xlab("Redundancy")+
+  ylab("Phylogenetic diversity")+
+  theme_bw(base_size = 14)+
+  theme(plot.title = element_text(face = "italic"))
